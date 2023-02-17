e3002215f7007c642ffb6b9b9829d570f276069e
fernando zuniga <carlosfernando.zh@gmail.com>
2023-02-16 21:01:23 -0600

    JS DOM practice

--- score_keeper/app.js
+++ score_keeper/app.js
@@ -2,6 +2,8 @@ const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+const resetButton = document.querySelector('#reset');
+const winingScoreselect = document.querySelector('#playTo');
 
 let p1Score = 0;
 let p2Score = 0;
@@ -36,3 +38,22 @@ p2Button.addEventListener('click', function (){
     p2Display.textContent = p2Score;
   }
 });
+
+winingScoreselect.addEventListener('change', function (){
+  winingScore = parseInt(this.value)
+  reset();
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset() {
+  isGamerOver = false;
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = p1Score;
+  p2Display.textContent = p2Score;
+  p2Display.style.color = 'black';
+  p1Display.style.color = 'black';
+  p1Button.disabled = false;
+  p2Button.disabled = false;
+}
