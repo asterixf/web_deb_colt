331357612afc3f220cba57201a3742a51a91a879
carlos herrera <carlosfernando.zh@gmail.com>
2023-04-13 21:46:19 -0600

    partials in express

--- templating_demo/index.js
+++ templating_demo/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.use(express.static(path.join(__dirname, "/public")));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -12,6 +14,11 @@ app.get("/", (req, res) => {
 app.get("/random", (req, res) => {
   const num = Math.floor(Math.random() * 10) + 1;
   res.render("random", { num });
+});
+
+app.get("/cats", (req, res) => {
+  const cats = ["meme", "monty", "sam", "jeanin"];
+  res.render("cats", { cats });
 });
 
 app.listen(3000, () => {});
